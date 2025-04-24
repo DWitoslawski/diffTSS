ddfea386f7e9f3543ecac7990ace529af7ffeca4
Daniel Witoslawski <witoslaw@unlv.nevada.edu>
2025-04-24 14:45:55 -0700

    fixing rna data incorporation

--- EPInformer/models.py
+++ EPInformer/models.py
@@ -186,7 +186,7 @@ class EPInformer_v2(nn.Module):
             self.seq_encoder = pre_trained_encoder
             self.name = 'EPInformerV2.preTrainedConv.{}base.{}dim.{}Trans.{}head.{}BN.{}LN.{}Feat.{}extraFeat.{}enh.{}RNA_enc.{}RNA_emb'.format(base_size, out_dim, n_encoder, head, useBN, useLN, useFeat, n_extraFeat, n_enhancer, rna_encoding, rna_embedding) 
         else:
-            self.seq_encoder = seq_256bp_encoder(base_size=base_size)
+            self.seq_encoder = seq_256bp_encoder(base_size=self.base_size)
             self.name = 'EPInformerV2.{}base.{}dim.{}Trans.{}head.{}BN.{}LN.{}Feat.{}extraFeat.{}enh.{}RNA_enc.{}RNA_emb'.format(base_size, out_dim, n_encoder, head, useBN,useLN, useFeat, n_extraFeat, n_enhancer, rna_encoding, rna_embedding)
         self.n_encoder = n_encoder
         self.device = device
