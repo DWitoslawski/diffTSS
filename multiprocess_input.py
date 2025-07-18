53a7f442ba193c225be111d1a7b9900770a71408
Daniel Witoslawski <witoslaw@unlv.nevada.edu>
2025-07-17 17:03:51 -0700

    fixing multiprocessing

--- multiprocess_input.py
+++ multiprocess_input.py
@@ -225,23 +225,23 @@ if __name__ == "__main__":
     mRNA_promoter_list = []
     PE_links_list = []
     rna_df_list = []
-    pool = Pool(processes=80)
-    for gene in tqdm(pool.imap(process_gene, gene_list), total=len(gene_list)):
+    with Pool(processes=80) as pool:
         if rna_method is not None:
-            pe_code, distance_list, activity_list, contact_list, mRNA_promoter_feat, gene_rna_df = gene
+            pe_code, distance_list, activity_list, contact_list, mRNA_promoter_feat, gene_rna_df = tqdm(pool.imap(process_gene, gene_list), total=len(gene_list))
             rna_df_list.append(gene_rna_df)
         else:
-            pe_code, distance_list, activity_list, contact_list, mRNA_promoter_feat = gene
+            pe_code, distance_list, activity_list, contact_list, mRNA_promoter_feat = tqdm(pool.imap(process_gene, gene_list), total=len(gene_list))
 
-        
+
         PE_code_list.append(pe_code)
         #PE_feat_list.append(PE_feat)
         PE_distance_list.append(distance_list)
         PE_activity_list.append(activity_list)
         PE_contact_list.append(contact_list)
         mRNA_promoter_list.append(mRNA_promoter_feat)
-    
-    pool.terminate()        
+
+    pool.close()
+    pool.join()
             
     PE_code_list = np.array(PE_code_list)
     #PE_feat_list = np.array(PE_feat_list)
