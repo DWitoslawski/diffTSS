4dcfdd84b4c7382b072849d2eef3cfc9759ae5ee
Daniel Witoslawski <witoslaw@unlv.nevada.edu>
2025-08-13 14:39:22 -0700

    fixing input generation

--- scripts/utils.py
+++ scripts/utils.py
@@ -223,11 +223,11 @@ def prepare_hd5_input(gene_enhancer_table, promoter_signals, gene_list, cells, n
                 if rna_method == 'embedding':
                     gene_rna_df = rna_df[rna_df[0] == gene]
 
-            PE_code, activity_list, distance_list, contact_list, gene_name, PE_links, gene_rna_df = encode_promoter_enhancer_links(gene_df, max_seq_len=2000, max_n_enhancer=60, max_distanceToTSS=100_000, add_flanking=False, rna_method=rna_method, rna_df=gene_rna_df_list)
+            PE_code, activity_list, distance_list, contact_list, gene_name, PE_links, gene_rna_df = encode_promoter_enhancer_links(gene_df, max_seq_len=2000, max_n_enhancer=60, max_distanceToTSS=100_000, add_flanking=False, rna_method=rna_method, rna_df_list=gene_rna_df_list)
             rna_signal_list.append(gene_rna_df)
 
         else:
-            PE_code, activity_list, distance_list, contact_list, gene_name, PE_links = encode_promoter_enhancer_links(gene_df, max_seq_len=2000, max_n_enhancer=60, max_distanceToTSS=100_000, add_flanking=False, rna_method=rna_method, rna_df=gene_rna_df_list)
+            PE_code, activity_list, distance_list, contact_list, gene_name, PE_links = encode_promoter_enhancer_links(gene_df, max_seq_len=2000, max_n_enhancer=60, max_distanceToTSS=100_000, add_flanking=False, rna_method=rna_method, rna_df_list=gene_rna_df_list)
         contact_list = np.concatenate([[0], contact_list])
         distance_list = np.concatenate([[0], distance_list/1000])
         activity_list = np.concatenate([[0], activity_list])
